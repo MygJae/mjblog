3cd1aaf30e955e4b32d2d3010b055fe0a50903ac
Mygjae <50861514+MygJae@users.noreply.github.com>
2024-03-25 22:55:58 +0900

    first commit

---
+++ src/test/java/com/mjblog/MjblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
