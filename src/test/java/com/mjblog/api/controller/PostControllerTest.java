e7e17031c39306d38b226edeb92c70fa4167198c
Mygjae <50861514+MygJae@users.noreply.github.com>
2024-03-30 22:19:05 +0900

    컨트롤러 생성

---
+++ src/test/java/com/mjblog/api/controller/PostControllerTest.java
@@ -0,0 +1,29 @@
+package com.mjblog.api.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/post 테스트")
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/post"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("ok"))
+                .andDo(print()); //http header 확인
+    }
+
+}
